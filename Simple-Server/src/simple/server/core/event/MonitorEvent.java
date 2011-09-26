42e10caa2670b2c48abfda89b3ac25dadabc3bb0
Javier A. Ortiz Bultr?n <javydreamercsw@users.noreply.github.com>
2011-09-26 11:22:42 -0500

    Remove monitor extension from normal distribution script.

--- Simple-Server/src/simple/server/core/event/MonitorEvent.java
+++ Simple-Server/src/simple/server/core/event/MonitorEvent.java
@@ -16,8 +16,10 @@ import simple.server.core.event.api.IMonitorEvent;
  */
 public class MonitorEvent extends SimpleRPEvent implements IMonitorEvent {
 
-    public static final String ACTION = "action", RPCLASS_NAME = "monitor_event", STRING = "string", OBJECT = "object";
-    public static final int GET_ZONE_INFO = 1, GET_ZONES = 2, REGISTER = 3, UNREGISTER = 4;
+    public static final String ACTION = "action", RPCLASS_NAME = "monitor_event", 
+            STRING = "string", OBJECT = "object";
+    public static final int GET_ZONE_INFO = 1, GET_ZONES = 2, REGISTER = 3, 
+            UNREGISTER = 4;
 
     public MonitorEvent(String string, int action) {
         super(RPCLASS_NAME);
