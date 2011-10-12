1ff682afacd84a7fdd49c670c13317855b82839e
Javier A. Ortiz Bultr?n <javydreamercsw@users.noreply.github.com>
2011-10-12 12:25:28 -0500

    Added JUnit tests and fixed classes based on test findings.

--- Card Game Extension/src/simple/server/extension/CardGameExtension.java
+++ Card Game Extension/src/simple/server/extension/CardGameExtension.java
@@ -2,7 +2,9 @@ package simple.server.extension;
 
 import marauroa.common.game.Definition;
 import marauroa.common.game.RPClass;
+import marauroa.common.game.RPObject;
 import org.openide.util.lookup.ServiceProvider;
+import simple.common.game.ClientObjectInterface;
 
 /**
  * Generates the RPClasses needed for adding decks to players.
@@ -12,9 +14,19 @@ import org.openide.util.lookup.ServiceProvider;
 @ServiceProvider(service = MarauroaServerExtension.class)
 public class CardGameExtension extends SimpleServerExtension {
 
+    public static final String DECK = "decks";
+
     @Override
     public void modifyClientObjectDefinition(RPClass player) {
         //Add decks to the player
-        player.addRPSlot("decks", Definition.PRIVATE);
+        player.addRPSlot(DECK, -1, Definition.PRIVATE);
+    }
+
+    @Override
+    public void clientObjectUpdate(ClientObjectInterface client) {
+        super.clientObjectUpdate(client);
+        if (!((RPObject) client).hasSlot(CardGameExtension.DECK)) {
+            ((RPObject) client).addSlot(CardGameExtension.DECK);
+        }
     }
 }
