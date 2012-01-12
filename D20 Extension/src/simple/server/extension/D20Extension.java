06526a1a488858ac97a1c0279708c921192f1955
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2012-01-12 14:49:21 -0600

    Move attributes to base entity as all entities will share those.

--- D20 Extension/src/simple/server/extension/D20Extension.java
+++ D20 Extension/src/simple/server/extension/D20Extension.java
@@ -4,7 +4,6 @@ import marauroa.common.game.Definition;
 import marauroa.common.game.RPClass;
 import marauroa.common.game.RPObject;
 import org.openide.util.lookup.ServiceProvider;
-import simple.common.game.ClientObjectInterface;
 
 /**
  *
@@ -23,58 +22,58 @@ public class D20Extension extends SimpleServerExtension {
             KNOWN_SPELLS = "Known Spells", PREP_SPELLS = "Prepared Spells";
 
     @Override
-    public void modifyClientObjectDefinition(RPClass player) {
-        player.addAttribute(TYPE, Definition.Type.STRING);
-        player.addAttribute(CLASS, Definition.Type.STRING);
-        player.addAttribute(TITLE, Definition.Type.STRING);
-        player.addAttribute(RACE, Definition.Type.STRING);
+    public void modifyRootRPClassDefinition(RPClass entity) {
+        entity.addAttribute(TYPE, Definition.Type.STRING);
+        entity.addAttribute(CLASS, Definition.Type.STRING);
+        entity.addAttribute(TITLE, Definition.Type.STRING);
+        entity.addAttribute(RACE, Definition.Type.STRING);
         //Stats
-        player.addAttribute(EXP, Definition.Type.INT);
-        player.addAttribute(AC, Definition.Type.INT);
-        player.addAttribute(INT, Definition.Type.INT);
-        player.addAttribute(CON, Definition.Type.INT);
-        player.addAttribute(DEX, Definition.Type.INT);
-        player.addAttribute(STR, Definition.Type.INT);
-        player.addAttribute(WIS, Definition.Type.INT);
-        player.addAttribute(CHA, Definition.Type.INT);
+        entity.addAttribute(EXP, Definition.Type.INT);
+        entity.addAttribute(AC, Definition.Type.INT);
+        entity.addAttribute(INT, Definition.Type.INT);
+        entity.addAttribute(CON, Definition.Type.INT);
+        entity.addAttribute(DEX, Definition.Type.INT);
+        entity.addAttribute(STR, Definition.Type.INT);
+        entity.addAttribute(WIS, Definition.Type.INT);
+        entity.addAttribute(CHA, Definition.Type.INT);
         //Other attributes
-        player.addRPSlot(SUBCLASS, -1);
-        player.addRPSlot(EQUIPMENT, -1);
-        player.addRPSlot(SKILLS, -1);
-        player.addRPSlot(FEATS, -1);
-        player.addRPSlot(ABILITIES, -1);
-        player.addRPSlot(SPELLS, -1);
-        player.addRPSlot(DEITIES, -1);
-        player.addRPSlot(DOMAIN, -1);
-        player.addRPSlot(KNOWN_SPELLS, -1);
-        player.addRPSlot(PREP_SPELLS, -1);
+        entity.addRPSlot(SUBCLASS, -1);
+        entity.addRPSlot(EQUIPMENT, -1);
+        entity.addRPSlot(SKILLS, -1);
+        entity.addRPSlot(FEATS, -1);
+        entity.addRPSlot(ABILITIES, -1);
+        entity.addRPSlot(SPELLS, -1);
+        entity.addRPSlot(DEITIES, -1);
+        entity.addRPSlot(DOMAIN, -1);
+        entity.addRPSlot(KNOWN_SPELLS, -1);
+        entity.addRPSlot(PREP_SPELLS, -1);
     }
 
     @Override
-    public void clientObjectUpdate(ClientObjectInterface client) {
-        if (!((RPObject) client).has(EXP)) {
-            ((RPObject) client).put(EXP, 0);
+    public void rootRPClassUpdate(RPObject client) {
+        if (!client.has(EXP)) {
+            client.put(EXP, 0);
         }
-        if (!((RPObject) client).has(INT)) {
-            ((RPObject) client).put(INT, 0);
+        if (!client.has(INT)) {
+            client.put(INT, 0);
         }
-        if (!((RPObject) client).has(CON)) {
-            ((RPObject) client).put(CON, 0);
+        if (!client.has(CON)) {
+            client.put(CON, 0);
         }
-        if (!((RPObject) client).has(DEX)) {
-            ((RPObject) client).put(DEX, 0);
+        if (!client.has(DEX)) {
+            client.put(DEX, 0);
         }
-        if (!((RPObject) client).has(STR)) {
-            ((RPObject) client).put(STR, 0);
+        if (!client.has(STR)) {
+            client.put(STR, 0);
         }
-        if (!((RPObject) client).has(WIS)) {
-            ((RPObject) client).put(WIS, 0);
+        if (!client.has(WIS)) {
+            client.put(WIS, 0);
         }
-        if (!((RPObject) client).has(CHA)) {
-            ((RPObject) client).put(CHA, 0);
+        if (!client.has(CHA)) {
+            client.put(CHA, 0);
         }
-        if (!((RPObject) client).has(AC)) {
-            ((RPObject) client).put(AC, 0);
+        if (!client.has(AC)) {
+            client.put(AC, 0);
         }
     }
 }
