c01109732c377c26482085a389b095fd1f15f72e
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2012-01-12 14:04:01 -0600

    Initial commit of the D20 extension. Just a dice roller implemented.

---
+++ D20 Extension/src/simple/server/extension/DiceRoller.java
@@ -0,0 +1,53 @@
+package simple.server.extension;
+
+import gmgen.plugin.DieEx;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * Simulates a dice roll
+ *
+ * @author Javier A. Ortiz Bultrón <javier.ortiz.78@gmail.com>
+ */
+@ServiceProvider(service = IDiceRoller.class)
+public class DiceRoller implements IDiceRoller {
+
+    private static final Logger logger = Logger.getLogger(DiceRoller.class.getSimpleName());
+    private static final Level level = Level.FINE;
+
+    public DiceRoller() {
+    }
+
+    @Override
+    public int roll(String diceExp) {
+        //Make sure there's an space between operators
+        diceExp = diceExp.replaceAll("\\+", " + ");
+        diceExp = diceExp.replaceAll("-", " - ");
+        logger.log(level, "Expression: {0}", diceExp);
+        int result = 0;
+        String[] tokens = diceExp.split("\\s");
+        String operator = null;
+        for (String token : tokens) {
+            String nextToken = token.trim().toLowerCase();
+            if (nextToken.contains("d")) {
+                //Is a dice expresion (i.e. 3d12)
+                result += new DieEx(nextToken).roll();
+            } else if (nextToken.equals("+") || nextToken.equals("-")) {
+                operator = nextToken;
+            } else {
+                //It should be an integer
+                float toAdd = Float.valueOf(nextToken);
+                if (operator.equals("+")) {
+                    logger.log(level, "Adding {0} to the result...", toAdd);
+                    result += toAdd;
+                } else if (operator.equals("-")) {
+                    logger.log(level, "Substracting {0} to the result...", toAdd);
+                    result -= toAdd;
+                }
+            }
+        }
+        logger.log(level, "Final Result: {0}", result);
+        return result;
+    }
+}
---
+++ D20 Extension/src/simple/server/extension/IDiceRoller.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple.server.extension;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultrón <javier.ortiz.78@gmail.com>
+ */
+public interface IDiceRoller {
+
+    int roll(String diceExp);
+    
+}
