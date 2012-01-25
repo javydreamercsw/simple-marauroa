1ef4991ec685f7e7006770b1e4841ec0801fee53
Administrator <Administrator@CT60820>
2012-01-25 19:46:05 +0100

    Fix tests.

--- Card Game Extension/test/simple/server/extension/DefaultCard.java
+++ Card Game Extension/test/simple/server/extension/DefaultCard.java
@@ -3,12 +3,21 @@ package simple.server.extension;
 import java.util.List;
 import javax.swing.ImageIcon;
 import org.openide.util.Lookup;
+import org.openide.util.lookup.AbstractLookup;
+import org.openide.util.lookup.InstanceContent;
 
 /**
  *
  * @author Javier A. Ortiz <javier.ortiz.78@gmail.com>
  */
 public class DefaultCard implements ICard {
+
+    private InstanceContent dynamicContent = new InstanceContent();
+    private Lookup myLookup = new AbstractLookup(dynamicContent);
+
+    public DefaultCard() {
+        dynamicContent.add(new DefaultType() {});
+    }
 
     @Override
     public List<ImageIcon> getImages() {
@@ -17,7 +26,7 @@ public class DefaultCard implements ICard {
 
     @Override
     public Lookup getLookup() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return myLookup;
     }
 
     @Override
