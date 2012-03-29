711d4892878a18ab022e3dedb4501a1bc66b0117
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.org>
2012-03-29 13:19:31 -0500

    javadoc

--- Card Game Interface/src/com/reflexit/magiccards/core/model/storage/IStorageContainer.java
+++ Card Game Interface/src/com/reflexit/magiccards/core/model/storage/IStorageContainer.java
@@ -2,5 +2,10 @@ package com.reflexit.magiccards.core.model.storage;
 
 public interface IStorageContainer<T> {
 
+    /**
+     * Get the storage
+     *
+     * @return storage
+     */
     IStorage<T> getStorage();
 }
