b51d8a0a921d71c5094a437f61968c2444538f92
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.org>
2012-03-29 13:21:27 -0500

    javadoc

--- Card Game Interface/src/com/reflexit/magiccards/core/model/IMergeable.java
+++ Card Game Interface/src/com/reflexit/magiccards/core/model/IMergeable.java
@@ -2,7 +2,17 @@ package com.reflexit.magiccards.core.model;
 
 public interface IMergeable<ICard> {
 
+    /**
+     * Set flag
+     *
+     * @param v flag
+     */
     public void setMergeOnAdd(boolean v);
 
+    /**
+     * Get flag's value
+     *
+     * @return flag
+     */
     public boolean getMergeOnAdd();
 }
