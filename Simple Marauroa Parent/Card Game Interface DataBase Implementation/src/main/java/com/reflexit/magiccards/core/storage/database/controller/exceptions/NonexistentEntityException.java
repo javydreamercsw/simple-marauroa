8d50c0b21d5beee6bb2a07661aebf16cd7b673bf
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2012-09-07 08:35:08 -0500

    Address some PMD warniongs.

--- Simple Marauroa Parent/Card Game Interface DataBase Implementation/src/main/java/com/reflexit/magiccards/core/storage/database/controller/exceptions/NonexistentEntityException.java
+++ Simple Marauroa Parent/Card Game Interface DataBase Implementation/src/main/java/com/reflexit/magiccards/core/storage/database/controller/exceptions/NonexistentEntityException.java
@@ -1,7 +1,5 @@
 package com.reflexit.magiccards.core.storage.database.controller.exceptions;
 
-import java.util.logging.Logger;
-
 public class NonexistentEntityException extends Exception {
     public NonexistentEntityException(String message, Throwable cause) {
         super(message, cause);
@@ -9,5 +7,4 @@ public class NonexistentEntityException extends Exception {
     public NonexistentEntityException(String message) {
         super(message);
     }
-    private static final Logger LOG = Logger.getLogger(NonexistentEntityException.class.getName());
 }
