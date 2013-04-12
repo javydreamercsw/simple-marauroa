7e851362bb78fef821c86663d9706e5ab5b9e5fe
jortiz00 <jortiz00@localhost>
2013-04-12 09:22:04 -0500

    PCGen, Remove Marauroa files

--- Simple Marauroa Parent/marauroa/src/marauroa/server/net/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The package stores the network related code.
- * <p>
- * It defines the interfaces for implementing a network manager ({@link marauroa.server.net.INetworkServerManager}) 
- * and the interface for implement a disconnection listener ({@link marauroa.server.net.IDisconnectedListener})
- */
-package marauroa.server.net;
-
