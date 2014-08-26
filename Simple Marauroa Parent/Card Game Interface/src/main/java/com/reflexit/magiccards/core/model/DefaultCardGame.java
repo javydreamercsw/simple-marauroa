d08e32c293cd253f7c999afe890c0b2121f5826c
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2014-08-26 08:58:40 -0500

    Minor refacturing/formatting.

--- Simple Marauroa Parent/Card Game Interface/src/main/java/com/reflexit/magiccards/core/model/DefaultCardGame.java
+++ Simple Marauroa Parent/Card Game Interface/src/main/java/com/reflexit/magiccards/core/model/DefaultCardGame.java
@@ -8,7 +8,6 @@ import java.awt.Image;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.logging.Level;
@@ -19,17 +18,22 @@ import org.openide.util.Lookup;
  *
  * @author Javier A. Ortiz Bultrón <javier.ortiz.78@gmail.com>
  */
-public abstract class DefaultCardGame implements ICardGame, DataBaseStateListener {
+public abstract class DefaultCardGame implements ICardGame,
+        DataBaseStateListener {
 
     protected static final List<String> attribs = new ArrayList<String>();
-    protected static final ArrayList<String> collectionTypes = new ArrayList<String>();
-    protected static final HashMap<String, String> collections = new HashMap<String, String>();
-    private static final Logger LOG = Logger.getLogger(DefaultCardGame.class.getName());
+    protected static final ArrayList<String> collectionTypes
+            = new ArrayList<String>();
+    protected static final HashMap<String, String> collections
+            = new HashMap<String, String>();
+    private static final Logger LOG
+            = Logger.getLogger(DefaultCardGame.class.getName());
 
     @Override
     public void init() {
         //Games auto register themselves
-        Lookup.getDefault().lookup(IDataBaseCardStorage.class).addDataBaseStateListener(this);
+        Lookup.getDefault().lookup(IDataBaseCardStorage.class)
+                .addDataBaseStateListener(this);
     }
 
     @Override
@@ -38,25 +42,29 @@ public abstract class DefaultCardGame implements ICardGame, DataBaseStateListene
         try {
             synchronized (attribs) {
                 //Create game attributes
-                for (Iterator<String> it = attribs.iterator(); it.hasNext();) {
-                    String attr = it.next();
-                    Lookup.getDefault().lookup(IDataBaseCardStorage.class).createAttributes(attr);
+                for (String attr : attribs) {
+                    Lookup.getDefault().lookup(IDataBaseCardStorage.class)
+                            .createAttributes(attr);
                 }
             }
             //Create default collection types
             synchronized (collectionTypes) {
-                for (Iterator<String> it = collectionTypes.iterator(); it.hasNext();) {
-                    String type = it.next();
-                    Lookup.getDefault().lookup(IDataBaseCardStorage.class).createCardCollectionType(type);
+                for (String type : collectionTypes) {
+                    Lookup.getDefault().lookup(IDataBaseCardStorage.class)
+                            .createCardCollectionType(type);
                 }
             }
             //Create default Collections
             synchronized (collections) {
-                for (Iterator<Entry<String, String>> it = collections.entrySet().iterator(); it.hasNext();) {
-                    Entry<String, String> entry = it.next();
+                for (Entry<String, String> entry : collections.entrySet()) {
                     parameters.put("name", entry.getKey());
-                    ICardCollectionType type = (ICardCollectionType) Lookup.getDefault().lookup(IDataBaseCardStorage.class).namedQuery("CardCollectionType.findByName", parameters).get(0);
-                    Lookup.getDefault().lookup(IDataBaseCardStorage.class).createCardCollection(type, entry.getValue());
+                    ICardCollectionType type
+                            = (ICardCollectionType) Lookup.getDefault()
+                            .lookup(IDataBaseCardStorage.class)
+                            .namedQuery("CardCollectionType.findByName",
+                                    parameters).get(0);
+                    Lookup.getDefault().lookup(IDataBaseCardStorage.class)
+                            .createCardCollection(type, entry.getValue());
                 }
             }
         } catch (DBException ex) {
@@ -67,10 +75,7 @@ public abstract class DefaultCardGame implements ICardGame, DataBaseStateListene
     @Override
     public List<ICardCache> getCardCacheImplementations() {
         ArrayList<ICardCache> caches = new ArrayList<ICardCache>();
-        for (Iterator<? extends ICardCache> it = 
-                Lookup.getDefault().lookupAll(ICardCache.class).iterator(); 
-                it.hasNext();) {
-            ICardCache cache = it.next();
+        for (ICardCache cache : Lookup.getDefault().lookupAll(ICardCache.class)) {
             if (cache.getGameName().equals(getName())) {
                 caches.add(cache);
             }
@@ -91,7 +96,8 @@ public abstract class DefaultCardGame implements ICardGame, DataBaseStateListene
     @Override
     public Image getGameIcon() {
         try {
-            return Lookup.getDefault().lookup(ICardCache.class).getGameIcon((ICardGame) this);
+            return Lookup.getDefault().lookup(ICardCache.class)
+                    .getGameIcon((ICardGame) this);
         } catch (IOException ex) {
             LOG.log(Level.SEVERE, null, ex);
             return null;
@@ -110,8 +116,10 @@ public abstract class DefaultCardGame implements ICardGame, DataBaseStateListene
 
     @Override
     public List<ICardAttributeFormatter> getGameCardAttributeFormatterImplementations() {
-        ArrayList<ICardAttributeFormatter> formatters = new ArrayList<ICardAttributeFormatter>();
-        for (ICardAttributeFormatter formatter : Lookup.getDefault().lookupAll(ICardAttributeFormatter.class)) {
+        ArrayList<ICardAttributeFormatter> formatters
+                = new ArrayList<ICardAttributeFormatter>();
+        for (ICardAttributeFormatter formatter : Lookup.getDefault()
+                .lookupAll(ICardAttributeFormatter.class)) {
             if (formatter.getGame().getName().equals(getName())) {
                 formatters.add(formatter);
             }
@@ -125,14 +133,17 @@ public abstract class DefaultCardGame implements ICardGame, DataBaseStateListene
         try {
             HashMap parameters = new HashMap();
             parameters.put("name", getName());
-            List result = Lookup.getDefault().lookup(IDataBaseCardStorage.class).namedQuery("Game.findByName", parameters);
+            List result = Lookup.getDefault().lookup(IDataBaseCardStorage.class)
+                    .namedQuery("Game.findByName", parameters);
             if (result.isEmpty()) {
-                throw new RuntimeException("Unable to find game " + getName() + " in database!");
+                throw new RuntimeException("Unable to find game " + getName()
+                        + " in database!");
             }
             parameters.clear();
-            return Lookup.getDefault().lookup(IDataBaseCardStorage.class).getSetsForGame((IGame) result.get(0));
+            return Lookup.getDefault().lookup(IDataBaseCardStorage.class)
+                    .getSetsForGame((IGame) result.get(0));
         } catch (DBException ex) {
-            Logger.getLogger(DefaultCardGame.class.getName()).log(Level.SEVERE, null, ex);
+            LOG.log(Level.SEVERE, null, ex);
             return new ArrayList<ICardSet>();
         }
     }
@@ -146,11 +157,13 @@ public abstract class DefaultCardGame implements ICardGame, DataBaseStateListene
             columns.add("Set");
             HashMap parameters = new HashMap();
             parameters.put("game", getName());
-            List result = Lookup.getDefault().lookup(IDataBaseCardStorage.class).createdQuery(
-                    "select distinct chca.cardAttribute from "
-                    + "CardHasCardAttribute chca, Card c, CardSet cs, Game g"
-                    + " where cs.game =g and g.name =:game and cs member of c.cardSetList"
-                    + " and chca.card =c order by chca.cardAttribute.name", parameters);
+            List result = Lookup.getDefault().lookup(IDataBaseCardStorage.class)
+                    .createdQuery(
+                            "select distinct chca.cardAttribute from "
+                            + "CardHasCardAttribute chca, Card c, CardSet cs, Game g"
+                            + " where cs.game =g and g.name =:game and cs member of c.cardSetList"
+                            + " and chca.card =c order by chca.cardAttribute.name",
+                            parameters);
             for (Object obj : result) {
                 ICardAttribute attr = (ICardAttribute) obj;
                 if (!columns.contains(attr.getName())) {
@@ -158,7 +171,7 @@ public abstract class DefaultCardGame implements ICardGame, DataBaseStateListene
                 }
             }
         } catch (DBException ex) {
-            Logger.getLogger(DefaultCardGame.class.getName()).log(Level.SEVERE, null, ex);
+            LOG.log(Level.SEVERE, null, ex);
         }
         return columns;
     }
