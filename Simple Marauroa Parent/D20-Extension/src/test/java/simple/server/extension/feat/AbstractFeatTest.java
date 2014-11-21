30e0e77d81122f850e4fa3b9e74c64a96c367011
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2014-11-21 16:51:32 -0600

    Add feats

---
+++ Simple Marauroa Parent/D20-Extension/src/test/java/simple/server/extension/feat/AbstractFeatTest.java
@@ -0,0 +1,31 @@
+package simple.server.extension.feat;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import simple.server.extension.DummySavingThrow;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+public class AbstractFeatTest {
+
+    public AbstractFeatTest() {
+    }
+
+    /**
+     * Test of getBonus method, of class AbstractFeat.
+     */
+    @Test
+    public void testGetBonus() {
+        System.out.println("getBonus");
+        AbstractFeat instance = new AbstractFeat();
+        instance.bonus.put(DummySavingThrow.class, "1");
+        assertEquals(1, instance.getBonus(DummySavingThrow.class));
+        instance.bonus.clear();
+        instance.bonus.put(DummySavingThrow.class, "2d+1");
+        assertTrue(instance.getBonus(DummySavingThrow.class) > 1);
+        instance.bonus.clear();
+        assertEquals(0, instance.getBonus(DummySavingThrow.class));
+    }
+}
