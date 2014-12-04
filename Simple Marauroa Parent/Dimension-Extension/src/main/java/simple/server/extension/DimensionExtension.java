22dabc12b33bc708800465e01aceff8c8823d485
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2014-12-04 10:57:10 -0600

    Refactoring and adding basic tests.

--- Simple Marauroa Parent/Dimension-Extension/src/main/java/simple/server/extension/DimensionExtension.java
+++ Simple Marauroa Parent/Dimension-Extension/src/main/java/simple/server/extension/DimensionExtension.java
@@ -12,52 +12,58 @@ import org.openide.util.lookup.ServiceProvider;
 @ServiceProvider(service = MarauroaServerExtension.class)
 public class DimensionExtension extends SimpleServerExtension {
 
+    public static final String X = "x", Y = "y", Z = "z",
+            RESISTANCE = "resistance", WIDTH = "width",
+            HEIGHT = "height", VISIBILITY = "visibility";
+
     @Override
     public void modifyRootRPClassDefinition(RPClass entity) {
         /**
          * Resistance to other entities (0-100). 0=Phantom, 100=Obstacle.
          */
-        entity.addAttribute("resistance", Definition.Type.BYTE, Definition.VOLATILE);
+        entity.addAttribute(RESISTANCE, Definition.Type.BYTE,
+                Definition.VOLATILE);
         /**
          * Entity location
          */
-        entity.addAttribute("x", Definition.Type.SHORT);
-        entity.addAttribute("y", Definition.Type.SHORT);
-        entity.addAttribute("z", Definition.Type.SHORT);
+        entity.addAttribute(X, Definition.Type.SHORT);
+        entity.addAttribute(Y, Definition.Type.SHORT);
+        entity.addAttribute(Z, Definition.Type.SHORT);
         /*
          * The size of the entity (in world units).
          */
-        entity.addAttribute("width", Definition.Type.SHORT, Definition.VOLATILE);
-        entity.addAttribute("height", Definition.Type.SHORT, Definition.VOLATILE);
+        entity.addAttribute(WIDTH, Definition.Type.SHORT, Definition.VOLATILE);
+        entity.addAttribute(HEIGHT, Definition.Type.SHORT, Definition.VOLATILE);
         /*
          * The visibility of the entity drawn on client (0-100). 0=Invisible,
          * 100=Solid. Useful when mixed with effect.
          */
-        entity.addAttribute("visibility", Definition.Type.INT, Definition.VOLATILE);
+        entity.addAttribute(VISIBILITY, Definition.Type.INT,
+                Definition.VOLATILE);
     }
 
     @Override
     public void rootRPClassUpdate(RPObject entity) {
-        if (!entity.has("width")) {
-            entity.put("width", 1);
+        if (!entity.has(WIDTH)) {
+            entity.put(WIDTH, 1);
         }
-        if (!entity.has("height")) {
-            entity.put("height", 1);
+        if (!entity.has(HEIGHT)) {
+            entity.put(HEIGHT, 1);
         }
-        if (!entity.has("x")) {
-            entity.put("x", 0);
+        if (!entity.has(X)) {
+            entity.put(X, 0);
         }
-        if (!entity.has("y")) {
-            entity.put("y", 0);
+        if (!entity.has(Y)) {
+            entity.put(Y, 0);
         }
-        if (!entity.has("z")) {
-            entity.put("z", 0);
+        if (!entity.has(Z)) {
+            entity.put(Z, 0);
         }
-        if (!entity.has("resistance")) {
-            entity.put("resistance", 100);
+        if (!entity.has(RESISTANCE)) {
+            entity.put(RESISTANCE, 100);
         }
-        if (!entity.has("visibility")) {
-            entity.put("visibility", 100);
+        if (!entity.has(VISIBILITY)) {
+            entity.put(VISIBILITY, 100);
         }
     }
 
