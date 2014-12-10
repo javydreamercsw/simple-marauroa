e3783cd962830e9707d3c990ff6f35f44e76b582
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2014-12-10 16:09:12 -0600

    Add alignments.

---
+++ Simple Marauroa Parent/D20-Extension/src/main/java/simple/server/extension/d20/alignment/Chaotic_Evil.java
@@ -0,0 +1,33 @@
+package simple.server.extension.d20.alignment;
+
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+@ServiceProvider(service = D20Alignment.class)
+public class Chaotic_Evil extends AbstractAlignment {
+
+    @Override
+    public String getDescription() {
+        return "A chaotic evil character does whatever his greed, hatred, "
+                + "and lust for destruction drive him to do. He is "
+                + "hot-tempered, vicious, arbitrarily violent, and "
+                + "unpredictable. If he is simply out for whatever he can "
+                + "get, he is ruthless and brutal. If he is committed to "
+                + "the spread of evil and chaos, he is even worse. "
+                + "Thankfully, his plans are haphazard, and any groups "
+                + "he joins or forms are poorly organized. Typically, "
+                + "chaotic evil people can be made to work together only "
+                + "by force, and their leader lasts only as long as he can "
+                + "thwart attempts to topple or assassinate him.\n"
+                + "\n"
+                + "Chaotic evil is sometimes called \"demonic\" because "
+                + "demons are the epitome of chaotic evil.\n"
+                + "\n"
+                + "Chaotic evil is the most dangerous alignment because "
+                + "it represents the destruction not only of beauty and "
+                + "life but also of the order on which beauty and life depend.";
+    }
+}
---
+++ Simple Marauroa Parent/D20-Extension/src/main/java/simple/server/extension/d20/alignment/Lawful_Good.java
@@ -0,0 +1,24 @@
+package simple.server.extension.d20.alignment;
+
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+@ServiceProvider(service = D20Alignment.class)
+public class Lawful_Good extends AbstractAlignment {
+
+    @Override
+    public String getDescription() {
+        return "A lawful good character acts as a good person is expected or "
+                + "required to act. She combines a commitment to oppose evil "
+                + "with the discipline to fight relentlessly. She tells the "
+                + "truth, keeps her word, helps those in need, and speaks out "
+                + "against injustice. A lawful good character hates to see the "
+                + "guilty go unpunished.\n"
+                + "\n"
+                + "Lawful good is the best alignment you can be because it "
+                + "combines honor and compassion.";
+    }
+}
