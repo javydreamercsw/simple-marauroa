8d37fe5886c62003c3704925c33921e59579fcf6
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2015-01-06 09:14:18 -0600

    Add basics for language.

--- Simple Marauroa Parent/D20-Extension/src/main/java/simple/server/extension/d20/language/D20Language.java
+++ Simple Marauroa Parent/D20-Extension/src/main/java/simple/server/extension/d20/language/D20Language.java
@@ -1,9 +1,11 @@
 package simple.server.extension.d20.language;
 
+import simple.server.extension.d20.D20Characteristic;
+
 /**
  *
  * @author Javier A. Ortiz Bultron javier.ortiz.78@gmail.com
  */
-public interface D20Language {
+public interface D20Language extends D20Characteristic{
     
 }
