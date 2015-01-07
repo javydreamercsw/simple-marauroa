6dba6dc1ce4102c2425267ceb6f68232742cc893
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2015-01-07 16:03:07 -0600

    Add requirements to skills as well.

--- Simple Marauroa Parent/D20-Extension/src/main/java/simple/server/extension/d20/skill/AbstractSkill.java
+++ Simple Marauroa Parent/D20-Extension/src/main/java/simple/server/extension/d20/skill/AbstractSkill.java
@@ -1,9 +1,12 @@
 package simple.server.extension.d20.skill;
 
+import java.util.ArrayList;
+import java.util.List;
 import marauroa.common.game.RPObject;
 import simple.server.extension.d20.dice.DieEx;
 import static simple.server.extension.d20.skill.D20Skill.modifiers;
 import simple.server.extension.d20.ability.D20Ability;
+import simple.server.extension.d20.rpclass.D20Class;
 
 /**
  *
@@ -12,6 +15,9 @@ import simple.server.extension.d20.ability.D20Ability;
 public abstract class AbstractSkill extends RPObject implements D20Skill {
 
     private Double rank = 0.0;
+    private List<Class<? extends D20Class>> exclusiveClasses
+            = new ArrayList<>();
+    private List<Class<? extends D20Skill>> requirements = new ArrayList<>();
 
     @Override
     public boolean isModifiesAttribute(Class<? extends D20Ability> attr) {
@@ -55,5 +61,15 @@ public abstract class AbstractSkill extends RPObject implements D20Skill {
     @Override
     public String getDescription() {
         return getClass().getSimpleName().replaceAll("_", " ");
+    }
+    
+    @Override
+    public List<Class<? extends D20Class>> getExclusiveClasses() {
+        return exclusiveClasses;
+    }
+
+    @Override
+    public List<Class<? extends D20Skill>> getRequirements() {
+        return requirements;
     }
 }
