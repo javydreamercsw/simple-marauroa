8ffc4fb8c390091428fd91de0f62aa5eeabe2211
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2015-01-15 11:44:50 -0600

    1) Some refactoring. 2) Fix issue with Level and Experience not showing up.

---
+++ Simple Marauroa Parent/D20-Extension/src/main/java/simple/server/extension/d20/list/FeatList.java
@@ -0,0 +1,41 @@
+package simple.server.extension.d20.list;
+
+import marauroa.common.game.Definition;
+import org.openide.util.lookup.ServiceProvider;
+import simple.server.extension.d20.list.AbstractList;
+import simple.server.extension.d20.list.D20List;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron javier.ortiz.78@gmail.com
+ */
+@ServiceProvider(service = D20List.class)
+public class FeatList extends AbstractList {
+
+    public static final String FEAT = "feats";
+
+    @Override
+    public int getSize() {
+        return -1;
+    }
+
+    @Override
+    public String getCharacteristicName() {
+        return FEAT;
+    }
+
+    @Override
+    public String getShortName() {
+        return FEAT;
+    }
+
+    @Override
+    public String getDescription() {
+        return "List of feats for this wrestler.";
+    }
+
+    @Override
+    public Byte getDefinition() {
+        return Definition.PRIVATE;
+    }
+}
