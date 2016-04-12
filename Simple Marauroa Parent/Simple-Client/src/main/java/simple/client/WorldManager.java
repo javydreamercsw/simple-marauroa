e1451f8248fce4911b631563c9166e12f1232f22
Javier Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2016-04-12 15:09:38 -0500

    Change interface and propagate changes.

--- Simple Marauroa Parent/Simple-Client/src/main/java/simple/client/WorldManager.java
+++ Simple Marauroa Parent/Simple-Client/src/main/java/simple/client/WorldManager.java
@@ -2,6 +2,8 @@ package simple.client;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import marauroa.common.game.RPObject;
 import org.openide.util.lookup.ServiceProvider;
 import simple.client.api.IWorldManager;
@@ -13,19 +15,30 @@ import simple.client.api.IWorldManager;
 @ServiceProvider(service = IWorldManager.class)
 public class WorldManager implements IWorldManager {
 
+    private static final Logger LOG
+            = Logger.getLogger(WorldManager.class.getSimpleName());
+
     private final Map<RPObject.ID, RPObject> world_objects = new HashMap<>();
 
     @Override
     public RPObject get(RPObject.ID id) {
-        if (world_objects != null) {
-            return world_objects.get(id);
-        } else {
-            return null;
-        }
+        return world_objects.get(id);
     }
 
     @Override
     public Map<RPObject.ID, RPObject> getWorld() {
         return world_objects;
+    }
+
+    @Override
+    public void showWorld() {
+        LOG.log(Level.INFO, "<World contents ------------------------------------->");
+        int j = 0;
+        for (RPObject object : world_objects.values()) {
+            j++;
+            LOG.log(Level.INFO, "{0}. {1}",
+                    new Object[]{j, object});
+        }
+        LOG.log(Level.INFO, "</World contents ------------------------------------->");
     }
 }
