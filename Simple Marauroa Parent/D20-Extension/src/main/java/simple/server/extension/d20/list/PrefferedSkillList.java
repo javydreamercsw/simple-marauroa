5ff627810eb04adabf2cf267b7ad0cfc961ae7d3
Javier Ortiz Bultron <javier.ortiz.78@gmail.com>
2016-06-21 12:56:06 -0500

    1) Fix bugs initializing classes. 2) Add missing lists.

---
+++ Simple Marauroa Parent/D20-Extension/src/main/java/simple/server/extension/d20/list/PrefferedSkillList.java
@@ -0,0 +1,40 @@
+package simple.server.extension.d20.list;
+
+import marauroa.common.game.Definition;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron javier.ortiz.78@gmail.com
+ */
+@ServiceProvider(service = D20List.class)
+public class PrefferedSkillList extends AbstractList {
+
+    public final static String NAME = "Preffered Skills",
+            DESC = "List of preffered skills for this character.";
+
+    @Override
+    public int getSize() {
+        return -1;
+    }
+
+    @Override
+    public String getCharacteristicName() {
+        return NAME;
+    }
+
+    @Override
+    public String getShortName() {
+        return NAME;
+    }
+
+    @Override
+    public String getDescription() {
+        return DESC;
+    }
+
+    @Override
+    public Byte getDefinition() {
+        return Definition.PRIVATE;
+    }
+}
