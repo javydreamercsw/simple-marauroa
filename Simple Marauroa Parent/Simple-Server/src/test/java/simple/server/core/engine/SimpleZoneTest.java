527445c5030f65feb630fa79c9bb359aba3466df
Javier A. Ortiz Bultr <javydreamercsw@users.noreply.github.com>
2016-09-16 15:10:11 -0500

    Minor improvement for tests.

--- Simple Marauroa Parent/Simple-Server/src/test/java/simple/server/core/engine/SimpleZoneTest.java
+++ Simple Marauroa Parent/Simple-Server/src/test/java/simple/server/core/engine/SimpleZoneTest.java
@@ -20,7 +20,7 @@ import simple.test.TestPlayer;
  */
 public class SimpleZoneTest extends AbstractSystemTest {
 
-    private IRPWorld world = Lookup.getDefault().lookup(IRPWorld.class);
+    private final IRPWorld world = Lookup.getDefault().lookup(IRPWorld.class);
 
     @Test
     public void testZoneCreationDeletion() {
@@ -48,7 +48,7 @@ public class SimpleZoneTest extends AbstractSystemTest {
         world.changeZone(zone.getID(), p2);
         world.changeZone(zone.getID(), p3);
         try {
-            Thread.sleep(1000);
+            Thread.sleep(5000);
         } catch (InterruptedException ex) {
             Logger.getLogger(SimpleZoneTest.class.getName())
                     .log(Level.SEVERE, null, ex);
