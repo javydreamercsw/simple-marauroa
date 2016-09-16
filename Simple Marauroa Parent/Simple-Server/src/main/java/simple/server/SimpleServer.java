779d4dd4613c616fa9d3f673cf4071e047793f85
Javier A. Ortiz Bultr <javier.ortiz.78@gmail.com>
2016-09-16 15:01:50 -0500

    Minor enhancements.

--- Simple Marauroa Parent/Simple-Server/src/main/java/simple/server/SimpleServer.java
+++ Simple Marauroa Parent/Simple-Server/src/main/java/simple/server/SimpleServer.java
@@ -18,22 +18,42 @@ public class SimpleServer {
             = Logger.getLogger(SimpleServer.class.getSimpleName());
     public static marauroad server;
 
-    public void startServer() {
-        if (server != null && server.isAlive()) {
-            Properties conf = new Properties();
-            //Load configuration file
-            File config = new File("server.ini");
-            //Load from file
-            try (FileInputStream in = new FileInputStream(config)) {
-                conf.load(in);
-                LOG.log(Level.INFO, "Loaded local properties from: {0}",
-                        config.getName());
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleServer ss = new SimpleServer();
+        if (args.length == 1) {
+            File config = new File(args[0]);
+            if (config.exists()) {
+                Properties conf = new Properties();
+                try (FileInputStream in = new FileInputStream(config)) {
+                    conf.load(in);
+                    LOG.log(Level.INFO, "Loaded local properties from: {0}",
+                            config.getName());
+                    ss.startServer(conf);
+                }
+                catch (IOException ex) {
+                    LOG.log(Level.SEVERE, "Error reading properties from disk!",
+                            ex);
+                }
+            }else{
+                LOG.log(Level.SEVERE, "Unable to find specified configuration "
+                        + "file: {0}", config);
             }
-            catch (IOException ex) {
-                LOG.log(Level.SEVERE, "Error reading properties from disk!",
-                        ex);
-            }
-            startServer(conf);
+        } else {
+            ss.startServer();
+        }
+    }
+
+    protected static void startCLI() {
+        new SimpleServerCLI().start();
+    }
+
+    public void stopServer() {
+        if (server != null) {
+            server.finish();
+            server = null;
         }
     }
 
@@ -42,25 +62,21 @@ public class SimpleServer {
         startCLI();
     }
 
-    public void stopServer() {
-        if (server != null && server.isAlive()) {
-            server.finish();
+    public void startServer() {
+        Properties conf = new Properties();
+        //Load configuration file
+        File config = new File("server.ini");
+        //Load from file
+        try (FileInputStream in = new FileInputStream(config)) {
+            conf.load(in);
+            LOG.log(Level.INFO, "Loaded local properties from: {0}",
+                    config.getName());
         }
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        SimpleServer ss = new SimpleServer();
-        ss.startServer();
-    }
-
-    protected void startCLI() {
-        new SimpleServerCLI().start();
-    }
-
-    public boolean isRunning() {
-        return server != null && server.isAlive();
+        catch (IOException ex) {
+            LOG.log(Level.SEVERE, "Error reading properties from disk!",
+                    ex);
+        }
+        server = marauroad.getMarauroa(conf);
+        startCLI();
     }
 }
