ae8623a7ab7b6580255cdc8b5ffb0e200a3cbbad
Javier A. Ortiz Bultr <javydreamercsw@users.noreply.github.com>
2016-09-19 11:41:55 -0500

    Add a protected method to inject default values .

--- Simple Marauroa Parent/Simple-Server/src/main/java/simple/server/DefaultINIGenerator.java
+++ Simple Marauroa Parent/Simple-Server/src/main/java/simple/server/DefaultINIGenerator.java
@@ -3,6 +3,8 @@ package simple.server;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Random;
 import java.util.logging.Logger;
@@ -15,8 +17,20 @@ import org.openide.util.lookup.ServiceProvider;
 public class DefaultINIGenerator implements INIGenerator {
 
     private RSAKey key;
+    protected Map<String, Object> defaults = new HashMap<>();
     private final static Logger LOG
             = Logger.getLogger(DefaultINIGenerator.class.getName());
+
+    public DefaultINIGenerator() {
+        defaults.put("database_adapter",
+                H2DatabaseAdapter.class.getCanonicalName());
+        defaults.put("tcp_port",
+                "" + new Random().nextInt(1000) * 3 + 1000);
+        defaults.put("jdbc_url",
+                "jdbc:h2:mem:;AUTO_RECONNECT=TRUE;DB_CLOSE_ON_EXIT=TRUE");
+        defaults.put("jdbc_class", Driver.class.getCanonicalName());
+        defaults.put("turn_length", "" + 50);
+    }
 
     @Override
     public File generateDefault() throws IOException {
@@ -29,14 +43,7 @@ public class DefaultINIGenerator implements INIGenerator {
             LOG.info("Generating minimal default configuration file...");
             out = new FileOutputStream(ini);
             //Add minimum required
-            p.put("database_adapter",
-                    H2DatabaseAdapter.class.getCanonicalName());
-            p.put("tcp_port",
-                    "" + new Random().nextInt(1000) * 3 + 1000);
-            p.put("jdbc_url",
-                    "jdbc:h2:mem:;AUTO_RECONNECT=TRUE;DB_CLOSE_ON_EXIT=TRUE");
-            p.put("jdbc_class", Driver.class.getCanonicalName());
-            p.put("turn_length", "50");
+            p.putAll(defaults);
             LOG.info("Generating encryption keys...");
             key = RSAKey.generateKey(512);
             p.put("e", key.getE().toString());
@@ -51,13 +58,6 @@ public class DefaultINIGenerator implements INIGenerator {
 
     @Override
     public File generateCustom() throws IOException {
-        Properties p = new Properties();
-        File ini = new File("server.ini");
-        if (ini.exists()) {
-            LOG.warning("File already exists, skipping!");
-        } else {
-
-        }
-        return ini;
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
