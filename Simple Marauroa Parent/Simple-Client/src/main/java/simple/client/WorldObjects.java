b19a74871d8e95d3d900fb808b6d021c1b0f3b97
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2017-01-17 08:33:54 -0600

    Formatting.

--- Simple Marauroa Parent/Simple-Client/src/main/java/simple/client/WorldObjects.java
+++ Simple Marauroa Parent/Simple-Client/src/main/java/simple/client/WorldObjects.java
@@ -1,4 +1,3 @@
-
 package simple.client;
 
 import java.util.ArrayList;
@@ -9,12 +8,13 @@ import java.util.List;
  * zones. Events shall be reported dependent on succession of game flow and in
  * particular free from events caused by perceptions and sync operations of the
  * lower client layers.
- * 
+ *
  * Currently works for zone events.
  */
 public class WorldObjects {
 
-    private static List<WorldListener> worldListeners = new ArrayList<WorldListener>();
+    private static final List<WorldListener> LISTENERS
+            = new ArrayList<WorldListener>();
 
     private WorldObjects() {
     }
@@ -24,69 +24,85 @@ public class WorldObjects {
      */
     public static interface WorldListener {
 
-        /** Called when a world zone has been loaded.
+        /**
+         * Called when a world zone has been loaded.
+         *
          * @param zoneName
          */
         void zoneEntered(String zoneName);
 
-        /** Called when a world zone has been unloaded.
+        /**
+         * Called when a world zone has been unloaded.
+         *
          * @param zoneName
          */
         void zoneLeft(String zoneName);
 
-        /** Called when the player arrives at a map location. */
+        /**
+         * Called when the player arrives at a map location.
+         */
         void playerMoved();
     }
 
-    /** Adds a WorldListener to this event distributor.
+    /**
+     * Adds a WorldListener to this event distributor.
+     *
      * @param a
      */
     public static void addWorldListener(WorldListener a) {
-        synchronized (worldListeners) {
-            if (!worldListeners.contains(a)) {
-                worldListeners.add(a);
+        synchronized (LISTENERS) {
+            if (!LISTENERS.contains(a)) {
+                LISTENERS.add(a);
             }
         }
     }
 
-    /** Removes a WorldListener from this event distributor.
+    /**
+     * Removes a WorldListener from this event distributor.
+     *
      * @param a
      */
     public static void removeWorldListener(WorldListener a) {
-        synchronized (worldListeners) {
-            worldListeners.remove(a);
+        synchronized (LISTENERS) {
+            LISTENERS.remove(a);
         }
     }
 
-    /** Create a zone-entered event.
+    /**
+     * Create a zone-entered event.
+     *
      * @param zoneName
      */
     public static void fireZoneEntered(String zoneName) {
-        synchronized (worldListeners) {
-            for (WorldListener wl : worldListeners) {
+        synchronized (LISTENERS) {
+            LISTENERS.forEach((wl) -> {
                 wl.zoneEntered(zoneName);
-            }
+            });
         }
     } // fireZoneEntered
 
-    /** Create a zone-left event.
+    /**
+     * Create a zone-left event.
+     *
      * @param zoneName
      */
     public static void fireZoneLeft(String zoneName) {
-        synchronized (worldListeners) {
-            for (WorldListener wl : worldListeners) {
+        synchronized (LISTENERS) {
+            LISTENERS.forEach((wl) -> {
                 wl.zoneLeft(zoneName);
-            }
+            });
         }
     } // fireZoneLeft
 
-    /** Create a player-moved event. */
+    /**
+     * Create a player-moved event.
+     */
     public static void firePlayerMoved() {
 
-        synchronized (worldListeners) {
-            for (WorldListener wl : worldListeners) {
+        synchronized (LISTENERS) {
+            LISTENERS.forEach((wl) -> {
                 wl.playerMoved();
-            }
+            });
         }
     } // firePlayerMoved
 }
